d0cbafdd7d8dcf47c99459da6535ea863af4bed3
KocTonpaB <33964138+KocTonpaB@users.noreply.github.com>
2018-09-13 19:57:59 +0300

    test_first
    add some scripts

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void LateUpdate()
+    {
+//        transform.LookAt(player);
+        transform.position.Set(player.position.x, player.position.y, -100);
+        Debug.Log(player.position.x);
+        Debug.Log(player.position.y);
+    }
+}
---
+++ Assets/Scripts/Mouving.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouving : MonoBehaviour
+{
+    public float speed = 9.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed; //"Horizontal" и "Vertical" - это допонительные имена для сопоставления с клавматурой
+        float deltaY = Input.GetAxis("Vertical") * speed;
+        transform.Translate(deltaX * Time.deltaTime, deltaY * Time.deltaTime, transform.position.z);
+    }
+}
