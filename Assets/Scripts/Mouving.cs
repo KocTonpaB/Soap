ec4c3bbc8e61bdca00c2ac48052095e3fc6d4270
KocTonpaB <33964138+KocTonpaB@users.noreply.github.com>
2018-09-14 22:05:33 +0300

    first techno demo
    add few sprites
    add new location
    add animation

--- Assets/Scripts/Mouving.cs
+++ Assets/Scripts/Mouving.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Mouving : MonoBehaviour
 {
     public float speed = 9.0f;
+    private Animator _animator;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        _animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -18,5 +19,7 @@ public class Mouving : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * speed; //"Horizontal" и "Vertical" - это допонительные имена для сопоставления с клавматурой
         float deltaY = Input.GetAxis("Vertical") * speed;
         transform.Translate(deltaX * Time.deltaTime, deltaY * Time.deltaTime, transform.position.z);
+        _animator.SetFloat("Vert", deltaY);
+        _animator.SetFloat("Hor", deltaX);
     }
 }
